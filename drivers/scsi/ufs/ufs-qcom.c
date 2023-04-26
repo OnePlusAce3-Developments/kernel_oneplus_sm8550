a5fc61ac052d36861010e8fde1aada581862e447
zhanghaojun1 <haojun.zhang@oppo.com>
2023-04-26 14:32:56 +0800

    [BSP.Kernel.Driver][1/1]{UFS clkscale hungtask workaround: disable wb toggling during clock scaling}
    适用范围：{Android 13/SM8550}
    准入id：{ OPPO-5551451 }
    分析：{UFS clkscale hungtask workaround: disable wb toggling during clock scaling}
    方案：{Remove UFSHCD_CAP_WB_WITH_CLK_SCALING to disable wb toggling during clock scaling}
    风险及影响[快/稳/省/功能/安全隐私]：{稳/快}
    测试建议：{正常开机}
    跨组依赖(topic name)：{}
    
    Change-Id: I040f23558beacb718b48c5ff173419f74e092a71
    Reviewed-on: http://gerrit.scm.adc.com:8080/21624060
    almid-check: oppo codebase <codebase@oppo.com>
    Reviewed-by: 张浩峻-80309504 <haojun.zhang@oppo.com>
    Tested-by: 张浩峻-80309504 <haojun.zhang@oppo.com>
    code-check: oppo_codecheck <oppo_codecheck@oppo.com>
    Reviewed-by: 欧阳润宇-80233393 <ouyangrunyu@oppo.com>
    Tested-by: 欧阳润宇-80233393 <ouyangrunyu@oppo.com>
    Reviewed-by: 王瑞-W9058479 <v-wangrui6@oppo.com>
    Tested-by: 王瑞-W9058479 <v-wangrui6@oppo.com>
    Reviewed-by: pswbuild <pswbuild@oppo.com>
    Tested-by: pswbuild <pswbuild@oppo.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -2213,7 +2213,10 @@ static void ufs_qcom_set_caps(struct ufs_hba *hba)
 	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
 
 	hba->caps |= UFSHCD_CAP_CLK_GATING | UFSHCD_CAP_HIBERN8_WITH_CLK_GATING;
-	hba->caps |= UFSHCD_CAP_CLK_SCALING | UFSHCD_CAP_WB_WITH_CLK_SCALING;
+	/* Haojun.Zhang@BSP.Kernel.Driver, UFS clkscaling hungtask problem workaround begin */
+	//hba->caps |= UFSHCD_CAP_CLK_SCALING | UFSHCD_CAP_WB_WITH_CLK_SCALING;
+	hba->caps |= UFSHCD_CAP_CLK_SCALING;
+	/* Haojun.Zhang@BSP.Kernel.Driver, UFS clkscaling hungtask problem workaround end */
 	hba->caps |= UFSHCD_CAP_AUTO_BKOPS_SUSPEND;
 	hba->caps |= UFSHCD_CAP_WB_EN;
 	hba->caps |= UFSHCD_CAP_CRYPTO;
