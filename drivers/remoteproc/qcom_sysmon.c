a6dd42952dca9e20570014c779f20e6018e7484d
W9065302 <v-wangrongfei@oppo.com>
2023-09-06 15:13:29 +0800

    [BSP.TP.Function][1/1]{OS14.0 解决PCR项目7550平台FC基线升级编译错误}
    适用范围：{androidU/sm7550/coloros14/benz}
    准入id：{6162872}
    分析：{基线升级编译错误}
    方案：{copy高通原生代码}
    风险及影响[快/稳/省/功能/安全隐私]：{无风险}
    测试建议：{无}
    跨组依赖(topic name)：{无}
    Signed-off-by: W9065302 <v-wangrongfei@oppo.com>
    
    Change-Id: I8fae0101eb9d8231590ef42565dbc730c4a5843a
    Reviewed-on: http://gerrit.scm.adc.com:8080/23942986
    almid-check: codebase <codebase@oppo.com>
    code-check: cqc <oppo_codecheck@oppo.com>
    Reviewed-by: 钟文杰-80225137 <zhongwenjie@oppo.com>
    Tested-by: 古启才-80249508 <guqicai@oppo.com>
    Reviewed-by: 古启才-80249508 <guqicai@oppo.com>

--- drivers/remoteproc/qcom_sysmon.c
+++ drivers/remoteproc/qcom_sysmon.c
@@ -638,6 +638,20 @@ static void sysmon_notif_timeout_handler(struct timer_list *t)
 		     subdevice_state_string[sysmon->state]);
 }
 
+static void sysmon_shutdown_notif_timeout_handler(struct timer_list *t)
+{
+	struct notif_timeout_data *td = from_timer(td, t, timer);
+	struct qcom_sysmon *sysmon = container_of(td, struct qcom_sysmon, timeout_data);
+
+	if (IS_ENABLED(CONFIG_QCOM_PANIC_ON_NOTIF_TIMEOUT) &&
+	    system_state != SYSTEM_RESTART &&
+	    system_state != SYSTEM_POWER_OFF &&
+	    system_state != SYSTEM_HALT &&
+	    !qcom_device_shutdown_in_progress)
+		panic(shutdown_timeout_msg, sysmon->name);
+	else
+		WARN(1, shutdown_timeout_msg, sysmon->name);
+}
 
 static inline void send_event(struct qcom_sysmon *sysmon, struct qcom_sysmon *source)
 {
@@ -722,8 +736,8 @@ static int sysmon_start(struct rproc_subdev *subdev)
 
 static void sysmon_stop(struct rproc_subdev *subdev, bool crashed)
 {
+	unsigned long timeout;
 	struct qcom_sysmon *sysmon = container_of(subdev, struct qcom_sysmon, subdev);
-	unsigned long timeout = 0;
 
 	trace_rproc_qcom_event(dev_name(sysmon->rproc->dev.parent), SYSMON_SUBDEV_NAME,
 			       crashed ? "crash stop" : "stop");
@@ -744,7 +758,7 @@ static void sysmon_stop(struct rproc_subdev *subdev, bool crashed)
 	if (crashed)
 		return;
 
-	sysmon->timeout_data.timer.function = sysmon_notif_timeout_handler;
+	sysmon->timeout_data.timer.function = sysmon_shutdown_notif_timeout_handler;
 	timeout = jiffies + msecs_to_jiffies(SYSMON_NOTIF_TIMEOUT);
 	mod_timer(&sysmon->timeout_data.timer, timeout);
 
