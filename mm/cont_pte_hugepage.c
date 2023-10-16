1fe44f44fd7fe7a0964bddc19190647e995e297f
wengle <wengle@oppo.com>
2023-10-16 20:36:02 +0800

    [BSP.Kernel.MM][1/1]{aston开启动态大页}
    适用范围：{[aston]|[23801]|[coloros14.0]}
    准入id：{OPPO-6374811}
    分析：{NA}
    方案：{NA}
    风险及影响[快/稳/省/功能/安全隐私]：{NA}
    测试建议：{NA}
    跨组依赖(topic name)：{无}
    
    Change-Id: I0d352b6d2a47c4d5e3d8b570a30dea64f8b6083b
    Reviewed-on: http://gerrit.scm.adc.com:8080/24827017
    almid-check: cqc <oppo_codecheck@oppo.com>
    code-check: cqc <oppo_codecheck@oppo.com>
    Reviewed-by: 吕闻环-80318990 <lvwenhuan@oppo.com>
    Reviewed-by: 翁乐-80379123 <wengle@oppo.com>
    Reviewed-by: 郑堂权-80370500 <zhengtangquan@oppo.com>
    rules-check: pswbuild <pswbuild@oppo.com>
    Reviewed-by: pswbuild <pswbuild@oppo.com>
    Tested-by: pswbuild <pswbuild@oppo.com>

--- mm/cont_pte_hugepage.c
+++ mm/cont_pte_hugepage.c
@@ -2985,7 +2985,7 @@ early_param("cmdline_cont_pte_sup_mem", cmdline_parse_cont_pte_sup_mem);
 static int __init cmdline_parse_prjname(char *p)
 {
 	static const char *cn_prjs[] = {
-		"22811", "21131", "22851",
+		"22811", "21131", "22851", "23801",
 		NULL,
 	};
 	static const char *other_prjs[] = {
