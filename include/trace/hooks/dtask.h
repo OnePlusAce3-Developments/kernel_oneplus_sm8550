2251c0ddfc5c7d88818f3c0235f2d24576a0184d
codebase <codebase@oppo.com>
2024-01-05 12:28:56 +0800

    [BSP.Kernel.Locking][1/1]{解决冲突}
    适用范围：{All}
    准入id：{6161412}
    分析：{升级基线有冲突}
    方案：{解决冲突}
    风险及影响[快/稳/省/功能/安全隐私]：{无}
    测试建议：{无}
    跨组依赖(topic name)：{无}
    
    Change-Id: I1e35c7d298a2356897dd91dff6871f0d72db864f
    Reviewed-on: http://gerrit.scm.adc.com:8080/26134357
    rules-check: pswbuild <pswbuild@oppo.com>
    almid-check: cqc <oppo_codecheck@oppo.com>
    Reviewed-by: 周学文-80382115 <zhouxuewen@oppo.com>
    Tested-by: 何鹰-80373228 <heying2@oppo.com>
    Reviewed-by: 何鹰-80373228 <heying2@oppo.com>
    code-check: cqc <oppo_codecheck@oppo.com>

--- include/trace/hooks/dtask.h
+++ include/trace/hooks/dtask.h
@@ -114,11 +114,6 @@ DECLARE_HOOK(android_vh_rtmutex_waiter_prio,
 	TP_PROTO(struct task_struct *task, int *waiter_prio),
 	TP_ARGS(task, waiter_prio));
 
-struct percpu_rw_semaphore;
-DECLARE_HOOK(android_vh_percpu_rwsem_wq_add,
-	TP_PROTO(struct percpu_rw_semaphore *sem, bool reader),
-	TP_ARGS(sem, reader));
-
 DECLARE_HOOK(android_vh_exit_signal_whether_wake,
 	TP_PROTO(struct task_struct *p, bool *wake),
 	TP_ARGS(p, wake));
